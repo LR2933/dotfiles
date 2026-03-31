85d980fdd9bed22f535ae5a9dd783bc66fe16e7a
LR2933 <r2933985616@gmail.com>
2026-03-31 23:32:56 +0800

    chore: 透明nvim背景

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,12 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    lazy = false,
     priority = 1000, -- 确保主题在其他插件之前加载
     config = function()
       require("catppuccin").setup({
         flavour = "mocha", -- 选填: latte, frappe, macchiato, mocha (默认)
-        transparent_background = false, -- 如果想要透明背景可以改为 true
+        transparent_background = true, -- 如果想要透明背景可以改为 true
         term_colors = true,
         integrations = {
           cmp = true,
@@ -21,9 +22,6 @@ return {
           -- 根据你使用的插件开启更多集成
         },
       })
-
-      -- 设置为当前主题
-      vim.cmd.colorscheme("catppuccin")
     end,
   },
   {
