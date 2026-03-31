c04b02c291f45f7fafb0c361a22d2d840ed09392
LR2933 <r2933985616@gmail.com>
2026-03-31 22:51:32 +0800

    feat: 添加了快捷键快速切换主题

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,5 @@ local keymap = vim.keymap.set
 keymap('n', '<leader>w', '<cmd>w<CR>', { silent = true, desc = "保存文件" })
 keymap('n', '<leader>q', '<cmd>q<CR>', { silent = true, desc = "退出" })
 keymap('i', 'jk', '<ESC>', { desc = "退出插入模式" })
+
+vim.keymap.set("n", "<leader>th", ":Telescope colorscheme enable_preview=true<CR>", { desc = "Switch Themes" })
