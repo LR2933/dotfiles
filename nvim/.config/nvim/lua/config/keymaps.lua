d7f84fd70001643c30454d9fbf63ce83cc5501fd
LR2933 <r2933985616@gmail.com>
2026-04-02 15:26:43 +0800

    chore(nvim): 更改快捷键绑定，取消silent
    leader + w 修改为仅修改后保存文件，没有修改就不保存

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,9 @@
 local keymap = vim.keymap.set
 
-keymap('n', '<leader>w', '<cmd>w<CR>', { silent = true, desc = "保存文件" })
-keymap('n', '<leader>q', '<cmd>q<CR>', { silent = true, desc = "退出" })
+keymap('n', '<leader>w', '<cmd>update<CR>', {desc = "保存(仅限更改后)"})
+
+keymap('n', '<leader>q', '<cmd>q<CR>', {desc = "退出" })
+
 keymap('i', 'jk', '<ESC>', { desc = "退出插入模式" })
 
 vim.keymap.set("n", "<leader>th", ":Telescope colorscheme enable_preview=true<CR>", { desc = "Switch Themes" })
