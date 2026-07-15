b3d219fb7a905f5aaeeddef46ad71272ee95c936
LR2933 <r2933985616@gmail.com>
2026-07-15 13:32:42 +0800

    feat(nvim): 开启隐藏符号

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -10,3 +10,11 @@ opt.clipboard = "unnamedplus" -- 使用系统剪贴板
 opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
+
+-- 1. 设置 Neovim 的隐藏级别为 2 
+-- 0: 不隐藏；1: 隐藏但保留占位符；2: 隐藏并替换为缩写或符号
+opt.conceallevel = 2
+
+-- 2. 配置 VimTeX 隐藏符号的范围
+-- a: 上下标, b: 粗体/斜体, d: 定界符(如括号), m: 数学符号, g: 希腊字母
+vim.g.tex_conceal = "abdmg"
