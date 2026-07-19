2d4db3de6da438715f5a884ca34a4d8e7e6d3990
LR2933 <r2933985616@gmail.com>
2026-07-19 09:44:44 +0800

    feat(nvim): 添加浮动终端
    leader + tt开启

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -19,5 +19,9 @@ return{
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
+    terminal = { enabled = true },  --浮动终端
   },
+  keys = {
+    { "<leader>tt", function () Snacks.terminal.open() end, desc = "Floating Terminal"},
+  }
 }
